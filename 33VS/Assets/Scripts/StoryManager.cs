58a5123e42768622c8c6375ee1648bda1325707a
christina2748394 <138764863+christina2748394@users.noreply.github.com>
2026-04-22 14:34:48 -0700

    added craft detect and target item detect

--- 33VS/Assets/Scripts/StoryManager.cs
+++ 33VS/Assets/Scripts/StoryManager.cs
@@ -6,11 +6,21 @@ public class StoryManager : MonoBehaviour
 {
     [SerializeField] public DialogueManager _startDialogue;
     // Start is called before the first frame update
+    public Slots _resultSlot;
+    [SerializeField] private CraftItems _targetItem;
 
     void Start()
     {
         _startDialogue.ShowStartDialogue();
         _startDialogue.AdvanceDialogue();
+
+    }
+    void Update()
+    {
+        if (_resultSlot._item = _targetItem)
+        {
+            Debug.Log("Target Item Get");
+        }
 
     }
 
